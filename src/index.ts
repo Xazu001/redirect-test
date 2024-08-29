9600ce0921dca1b56a2189fc53ff7fa5c425b7e9
Xazu <Xazu001@users.noreply.github.com>
2024-08-29 14:37:09 +0200

    ws

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import WebSocket from "ws";
+import { createNodeWebSocket } from "@hono/node-ws";
 
 const app = new Hono();
+
+const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app });
 
 const adresKacpra = "25.17.43.102";
 const ws = new WebSocket(`ws://${adresKacpra}/ws`);
 
-app.get("/redirect", (c) => {
-  ws.send("Hello N!");
-
-  return c.text("Hello Hono!");
-});
+app.get(
+  "/redirect",
+  upgradeWebSocket((c) => {
+    ws.send("Hello from server!");
+    return {
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`);
+        ws.send("Hello from server!");
+      },
+      onClose: () => {
+        console.log("Connection closed");
+      },
+    };
+  })
+);
 
 const port = 5173;
 console.log(`Server is running on port ${port}`);
